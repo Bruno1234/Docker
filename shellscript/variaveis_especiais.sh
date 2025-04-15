4a15713daf3ea8c18d3e78d6fbc04c4935253dc5
Bruno <brunozuerodog@gmail.com>
2025-04-15 08:24:52 -0400

    featividade 14/04

---
+++ shellscript/variaveis_especiais.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#variaveis especiais que capturam parametros passados na execucao do script
+
+echo "Qtde de argumetos: $#:"
+echo "Nome do arquivo: $0"
+echo "Primeiro parametro: $1:"
+echo "Segundo parametro: $2:"
+echo "Terceiro parametro: $3:"

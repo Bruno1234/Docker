0e3fedb9d14d49e906c56e56d4766fdadfef7d25
Bruno <brunozuerodog@gmail.com>
2025-04-15 07:49:17 -0400

    fear: Shell script básico

---
+++ shellscript/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+apt update
+apt -y upgrade 
+apt -y install nano git
+git clone https://github.com/Bruno1234/Docker
+cd Docker 
+ls
---
+++ shellscript/variaveis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+clear
+nome='Bruno'
+idade=22
+echo Hello $nome de idade $idade!!
